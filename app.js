ff6692e6929b66d4540363c9d9151f7143357f89
Bapu <1,bapu.ojha@gmail.com>
2022-10-08 22:05:41 +0530

    3rd commit

--- app.js
+++ app.js
@@ -18,17 +18,17 @@ app.post("/", (req, res) => {
     const link = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2c2668ffce38642ac87dd9889496413d&units=metric`
 
     https.get(link, (response) => {
-        // console.log(response.statusCode);
+        console.log(response.weather);
         response.on('data', (data) => {
             const weatherData = JSON.parse(data);
             const icon = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
             res.send(`<h1> you are in ${weatherData.name} and the weather is ${weatherData.weather[0].description} </h1><br>
             <h2> the temperature is ${weatherData.main.temp} degrees celcius</h2> <br>
             <img src= ${icon} >`)
-            // res.send();
         })
     })
 })
+
 
 
 app.listen(5000, () => {
